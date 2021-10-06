c53e283c3e1cf58c7ef1c890c69049317817f2a6
michelle <michelle.watkins@engineering.digital.dwp.gov.uk>
2021-10-06 15:56:59 +0100

    test

--- Gemfile
+++ Gemfile
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
+
+ruby '3.0.2'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'rspec'
 gem 'webrick'
+gem 'capybara'
--- app.rb
+++ app.rb
@@ -1,14 +1,12 @@
 require 'sinatra/base'
-require "sinatra/reloader"
+require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
-  configure :development do
-    register Sinatra::Reloader
-  end  
 
   get '/' do
-    "Hello Battle!"
+    'Testing infrastructure working!'
   end
 
   run! if app_file == $0
-end  
+
+end
